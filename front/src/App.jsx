cb7d706da8f74f6f1c6aea22a953d89d7b57d71f
mhambardzumyan675_ui <mhambardzumyan675@gmail.com>
2026-09-01 21:51:58 +0400

    Home Header

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter,Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
-import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import './App.scss'
 
@@ -9,7 +8,6 @@ const App = () => {
   return (
     <>
    <BrowserRouter>
-   <Header/>
    <Routes>
     <Route path='/' element={<Home/>}/>
    </Routes>
--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import './Home.scss'
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <></>
+    <>
+    <div className='home-page-bcg'>
+      <div className='container'>
+      <header className='header'>
+        <div className='page-logo'>
+          <img className='logo-pic' src='./images/logo.png'></img>
+        </div>
+        <nav className='header-navlist'>
+            <ul className='navlist'>
+                <li className='nav-item'><Link to='/list' >List your property</Link></li>
+                <li className='nav-item'><Link to='/support'>Support</Link></li>
+                <li className='nav-item'><Link to='/trips' >Trips</Link></li>
+                <li className='nav-item'><Link to='/signin' >Sign in</Link></li>
+            </ul>
+        </nav>
+        <div><button className='app_btn' >Get the app</button></div>
+    </header>
+    </div>
+    </div>
+    </>
   )
 }
 
